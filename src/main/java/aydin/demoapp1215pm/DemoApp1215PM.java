44e84a50b648a3802ea74d47bfe0c664731ebff9
ilknur <ilknur@137.125.155.32>
2022-05-10 13:15:13 -0400

    first upload

---
+++ src/main/java/aydin/demoapp1215pm/DemoApp1215PM.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package aydin.demoapp1215pm;
+
+/**
+ *
+ * @author ilknur
+ */
+public class DemoApp1215PM {
+
+    public static void main(String[] args) {
+        System.out.println("This is a two player computer game, creating players....");
+        Player p1 = new Player("Aliza", 0);
+        Player p2 = new Player("Stravinsky", 0);
+        System.out.println("Created players...");
+    }
+}
---
+++ src/main/java/aydin/demoapp1215pm/Player.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aydin.demoapp1215pm;
+
+/**
+ *
+ * @author ilknur
+ */
+public class Player {
+    // private fields
+    private String name;
+    private int score;
+    
+    // constructor with parameters
+    public Player(String theName, int theScore){
+        name = theName;
+        score = theScore;
+    }
+    
+    // getters
+    
+    // setters
+    
+    // ToString method
+    
+    
+}
